335bdb6c546134e41214215ae1de0fa0c1bfbda6
Nastase Alexandru <nastasealexandru22@stud.ase.ro>
2025-01-03 23:25:45 +0200

    commit

---
+++ database/AddDailyScript.py
@@ -0,0 +1,19 @@
+from stock.Stock import *
+import pandas as pd
+from stock.EvalutateStock import *
+from database.PopulateDB import PopulateDB
+from utils.SaveDividendData import SaveDocsData
+
+url = "https://docs.google.com/spreadsheets/d/1D4H2OoHOFVPmCoyKBVCjxIl0Bt3RLYSz/export?format=csv&gid=330805790#gid=330805790"
+saved_csv_file_path = "outData/dividend_spreadsheet.csv"
+new_csv_file_path = "outData/filtered_sorted_companies.csv"
+
+obj = SaveDocsData(url)
+obj.save_data(saved_csv_file_path)
+obj.process_data(saved_csv_file_path,new_csv_file_path)
+
+list_companies = pd.read_csv(new_csv_file_path)
+list_companies = list_companies['Symbol'].tolist()
+
+populate = PopulateDB('companies.db')
+populate.populate_all(list_companies)
