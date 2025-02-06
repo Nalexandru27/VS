5e5d48a481c1372368906ba42cdab83338859287
Nastase Alexandru <nastasealexandru22@stud.ase.ro>
2025-02-06 22:35:22 +0200

    commit

--- stock/Stock.py
+++ stock/Stock.py
@@ -99,7 +99,7 @@ class Stock:
     # Get balance sheet
     def get_balance_sheet(self):
         try:
-            url = f'https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol={self.ticker}&apikey=H03ZN6G32VUTNCGT'
+            url = f'https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol={self.ticker}&apikey=WYGPKB8T21WMM6LO'
             r = requests.get(url)
             data = r.json()
             annual_reports = data['annualReports']
