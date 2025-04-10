ff4d85cb6ed9e8cb64be51d2088d261ae92f7d53
Nalexandru27 <79186873+Nalexandru27@users.noreply.github.com>
2025-04-11 00:28:55 +0300

    commit

--- utils/SaveDailyPrices/InsertDailyPricesIntoDB.py
+++ utils/SaveDailyPrices/InsertDailyPricesIntoDB.py
@@ -7,14 +7,26 @@ import pandas as pd
 import Constants as cnst
 from database.DatabaseCRUD import DatabaseCRUD
 from database.DatabaseConnection import db_connection
-from utils.GoogleDriveDB import upload_file_to_drive, download_file_from_drive
+from utils.GoogleDriveDB import upload_file_to_drive, download_file_from_drive, list_files_in_folder
 
 database_path = os.path.normpath(os.path.join(os.path.dirname(__file__), "..", "..", "companies.db"))
+
 def insert_daily_prices():
+    # Listează fișierele pentru debugging
+    print("Verificăm fișierele din folderul Google Drive:")
+    files = list_files_in_folder()
+    
+    # Afișează calea exactă unde se va descărca baza de date
+    print(f"Calea pentru descărcare: {os.path.abspath(database_path)}")
+    
+    # Afișează numele exact al fișierului de backup căutat
+    backup_filename = "backup_" + cnst.DB_NAME
+    print(f"Căutăm fișierul: {backup_filename}")
+
     df = pd.read_csv(cnst.FILLED_DAILY_PRICE_FILE_PATH, index_col=0)
     if download_file_from_drive("backup_" + cnst.DB_NAME, database_path):
         print(f"Latest database version of {cnst.DB_NAME} was downloaded successfully at {database_path}")
-        time.sleep(15)
+        time.sleep(20)
         db_crud = DatabaseCRUD()
         yestarday = pd.Timestamp.today() - pd.Timedelta(days=1)
         formatted_yestarday = yestarday.strftime("%Y-%m-%d")
@@ -27,6 +39,7 @@ def insert_daily_prices():
         db_connection.close_connection()
         print(f"Daily prices inserted successfully into database {cnst.DB_NAME}.")
 
+        time.sleep(10)
         upload_file_to_drive(database_path, cnst.DB_NAME)
         print(f"Updated database {cnst.DB_NAME} was successfully uploaded to Google Drive")
     else:
